819385930df65419316ae23da73c81d441e4b7f2
Ettore Hidoux <ettore.hidoux@imt-atlantique.net>
2023-02-17 15:50:09 +0100

    update watermark reconstruction

--- content/watermark/secret_matrix.py
+++ content/watermark/secret_matrix.py
@@ -54,3 +54,17 @@ def get_watermark_from_text(text: str) -> np.array:
     """
     binary_str = ''.join(format(ord(c), '08b') for c in text)
     return [int(b) for b in binary_str]
+
+def get_text_from_watermark(watermark: np.array) -> str:
+    """_summary_
+
+    Args:
+        watermark (np.array): array of either 0 or 1
+
+    Returns:
+        str: the embbeded text
+    """
+    character = [str(bit) for bit in watermark]
+    character = [character[i*8:(i+1)*8] for i in range(len(character)//8)]
+    character = [chr(int(''.join(char), 2)) for char in character]
+    return ''.join(character)
